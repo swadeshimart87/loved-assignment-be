54c1ddd291b3c5771d9a7fffff28dfd966267791
swadeshimart87 <72510759+swadeshimart87@users.noreply.github.com>
2021-07-07 12:29:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ var yahooFinance = require('yahoo-finance');
 
 const port = process.env.PORT || 5000;
 
-app.get('/app', (req, res) => {
+app.get('/', (req, res) => {
   const SYMBOLS = req.query && req.query.symbols && req.query.symbols.split(',');
-  yahooFinance.quote({
-    symbols: SYMBOLS,
-    modules: ['price']
-  }, function(err, quotes) {
-    if(err) {
-      res.status(500).send(err);
+  if(SYMBOLS) {
+    yahooFinance.quote({
+      symbols: SYMBOLS,
+      modules: ['price']
+    }, function(err, quotes) {
+      if(err) {
+        res.status(500).send(err);
+        return;
+      }
+      res.status(200).send(quotes)
       return;
-    }
-    res.status(200).send(quotes)
-    return;
-  });
+    });
+  } else {
+    res.status(400).send({'Error': 'Send symbols'});
+  }
 })
 
 app.listen(port, () => {
