595910a86787d4c00cac94912c0f2c94ed562472
swadeshimart87 <72510759+swadeshimart87@users.noreply.github.com>
2021-07-07 12:41:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var yahooFinance = require('yahoo-finance');
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   const SYMBOLS = req.query && req.query.symbols && req.query.symbols.split(',');
   if(SYMBOLS) {
     yahooFinance.quote({
@@ -21,7 +21,16 @@ app.get('/', (req, res) => {
   } else {
     res.status(400).send({'Error': 'Send symbols'});
   }
-})
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+};
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
